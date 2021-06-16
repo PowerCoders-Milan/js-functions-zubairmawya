55bce3b39acd1f800e4a797d9bd4ca9646eb7e0d
Zubair Khan <zubairmawya@gmail.com>
2021-06-16 12:12:18 +0200

    can you vote

--- 01-can_you_vote/can_you_vote.js
+++ 01-can_you_vote/can_you_vote.js
@@ -1,15 +1,21 @@
 // define a function that will check if I can vote or not
 const canIVote = age => {
     // define a variable legalAge
-
+const legalAge = 18;
     // create if else statement that console.log() if you can vote or not
-    if (condition) {
-        
+    if (age >= legalAge) {
+     console.log("you can vote");   
     } else {
-        
+     console.log("you are to young"); 
     }
 }
 
 // define a variable for your age
 
 // call the function and see if you can vote
+
+canIVote(30)
+canIVote(17)
+canIVote(22)
+canIVote(17)
+canIVote(20)
